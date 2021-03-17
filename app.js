b23035ec9f3e6c6829341c76104769a4d81a0b90
qzhang0226 <zhangqvivi@gmail.com>
2021-03-17 15:43:55 -0400

    middleware

--- app.js
+++ app.js
@@ -1,58 +1,60 @@
 const path = require("path");
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv/config');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 
 const app = express();
 
 // Middlewares
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(bodyParser.json());
-app.use('/uploads', express.static(path.join('uploads')));
+app.use("/uploads", express.static(path.join("uploads")));
 
 // Home
-const homeRoute = require('./routes/home');
-app.use('/home', homeRoute);
+const homeRoute = require("./routes/home");
+app.use("/home", homeRoute);
 
 // About
-const aboutRoute = require('./routes/about');
-app.use('/about', aboutRoute);
+const aboutRoute = require("./routes/about");
+app.use("/about", aboutRoute);
 
 // Profile
-const profileRoute = require('./routes/profile');
-app.use('/profile', profileRoute);
+const profileRoute = require("./routes/profile");
+app.use("/profile", profileRoute);
 
 // Experience
-const experienceRoute = require('./routes/experience');
-app.use('/experience', experienceRoute);
+const experienceRoute = require("./routes/experience");
+app.use("/experience", experienceRoute);
 
 // Project
-const projectRoute = require('./routes/project');
-app.use('/project', projectRoute);
+const projectRoute = require("./routes/project");
+app.use("/project", projectRoute);
 
 // Quotation
-const quoteRoute = require('./routes/quotation');
-app.use('/quotation', quoteRoute);
+const quoteRoute = require("./routes/quotation");
+app.use("/quotation", quoteRoute);
 
 // Contact
-const contactRoute = require('./routes/contact');
-app.use('/contact', contactRoute);
+const contactRoute = require("./routes/contact");
+app.use("/contact", contactRoute);
 
 // Blogs
-const blogsRoute = require('./routes/blogs');
-app.use('/blog', blogsRoute);
+const blogsRoute = require("./routes/blogs");
+app.use("/blog", blogsRoute);
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('We are on home!')
-})
+app.get("/", (req, res) => {
+  res.send("We are on home!");
+});
 
-mongoose.connect(
-    process.env.MONGODB_URI,
-    { useNewUrlParser: true }, 
-    () => console.log('connected to DB')    
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () =>
+  console.log("connected to DB")
 );
 
 module.exports = app;
